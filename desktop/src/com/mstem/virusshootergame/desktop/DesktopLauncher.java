e606fc36f232988cf941df202397eaf61620af2e
Catherine Huang <c_huang4@post.massbay.edu>
2015-04-13 22:24:57 -0400

    inital

---
+++ desktop/src/com/mstem/virusshootergame/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.mstem.virusshootergame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mstem.virusshootergame.MainClass;
+import com.mstem.virusshootergame.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1000;
+        config.height = 558;
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+
+
+}
---
+++ html/src/com/mstem/virusshootergame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mstem.virusshootergame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mstem.virusshootergame.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGdxGame();
+        }
+}
